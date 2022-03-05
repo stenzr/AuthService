96d3a230261fbc3bbe49ff416a0a8a4531385228
stenzr <rkrohitkr01@gmail.com>
2022-03-05 14:25:23 +0530

    [ADD] user schema

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Schema of the collection
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    emaul: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+// Expose the Schema as a model
+module.exports = User = mongoose.model("users".UserSchema);
